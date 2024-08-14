f92ad1b3717d86d2ee7f72f3a5e60e2fcca3d21e
UTKARSH-DEVLOP <guptautkarsh212001@gmail.com>
2024-08-14 16:04:02 +0530

    added a screen for game

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import sys 
+import pygame
+  
+class AlienInvasion:
+    """ Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        """Initialize the game,  and create game resources"""    
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230,230,230)
+
+    def run_game(self):
+        """start the main loop for the game"""
+
+        while True:
+            #watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.screen.fill(self.bg_color)
+            pygame.display.flip()
+if __name__ == '__main__':
+    ai=AlienInvasion()
+    ai.run_game()
